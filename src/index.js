73ce2e836834329044dc4753862a207a1c3bee6b
youbaidjis <youba.etud@gmail.com>
2022-06-18 22:59:07 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,16 +1,91 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, NavLink, Route} from "react-router-dom";
 
-function Home() {
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "12px",
+  margin: "0 6px 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
+
+
+function NavBar() {
   return (
     <div>
-      <h1>Home!</h1>
+      <NavLink
+        to="/"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/about"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        About
+      </NavLink>
+      <NavLink
+        to="/login"
+        exact
+        style={linkStyles}
+        activeStyle={{
+          background: "darkblue",
+        }}
+      >
+        Login
+      </NavLink>
     </div>
   );
 }
 
-function App() {
-  return <Home />;
+function Home() {
+  return <h1>Home!</h1>;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function About() {
+  return <h1>This is my about component!</h1>;
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+   
+   <Routes>
+    
+    <Route path="/about" element={<About />}></Route>
+    <Route path="/login" element={ <Login />}></Route>
+    <Route path="/" element={<Home />}></Route>
+
+    </Routes>
+    
+  </BrowserRouter>,
+  document.getElementById("root")
+);
