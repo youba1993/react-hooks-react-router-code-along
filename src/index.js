1984f5790338ef021540e6ed391843f04676f460
youbaidjis <youba.etud@gmail.com>
2022-06-19 18:31:35 -0700

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,91 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, NavLink, Route} from "react-router-dom";
-
-const linkStyles = {
-  display: "inline-block",
-  width: "50px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
-
-
-function NavBar() {
-  return (
-    <div>
-      <NavLink
-        to="/"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Home
-      </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/login"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        Login
-      </NavLink>
-    </div>
-  );
-}
-
-function Home() {
-  return <h1>Home!</h1>;
-}
-
-function About() {
-  return <h1>This is my about component!</h1>;
-}
-
-function Login() {
-  return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <div>
-          <input type="text" name="username" placeholder="Username" />
-        </div>
-        <div>
-          <input type="password" name="password" placeholder="Password" />
-        </div>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
+import { BrowserRouter } from "react-router-dom";
+import App from "./components/App";
 
 ReactDOM.render(
   <BrowserRouter>
-   
-   <Routes>
-    
-    <Route path="/about" element={<About />}></Route>
-    <Route path="/login" element={ <Login />}></Route>
-    <Route path="/" element={<Home />}></Route>
-
-    </Routes>
-    
+    <App />
   </BrowserRouter>,
   document.getElementById("root")
 );
